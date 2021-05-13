7a817aa5e0031c70eb6f9a02a4c3f689928e3600
Hayden-coder <hayden377@hotmail.com>
2021-05-13 14:11:17 -0700

    added code

---
+++ src/resumeData.js
@@ -0,0 +1,69 @@
+let resumeData = {
+    "imagebaseurl":"https://hayden-coder.github.io/Updated-Portfolio-Page-2/",
+    "name": "Hayden Vennes",
+    "role": "Full stack web developer",
+    "linkedinId":"Hayden377@hotmail.com",
+    "roleDescription": "Developing front end website architecture.Designing user interactions on web pages.Developing back-end website applications.Creating servers and databases for functionality.Ensuring cross-platform optimization for mobile phones.Ensuring responsiveness of applications.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/hayden-vennes-2454381bb/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Hayden-coder",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"My name is hayden im a full stack developer. I am learning HTML5, CSS3, JavaScript, Bootstrap, JQuery, AJAX, Git, GitBash, NodeJS,ExpressJS, MySQL, MongoDB.I'm outgoing and follow instructions. I love to play Basketball, play video games, travel around the world, watching movies and reading comic books.",
+    "address":"Seattle",
+    "website":"https://github.com/Hayden-coder",
+    
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"cure for boredom",
+        "imgurl":"https://walrus-assets.s3.amazonaws.com/img/Kingwell_Boredom-e1562811472249.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"Deja-brew",
+        "imgurl":"https://lh3.googleusercontent.com/proxy/Zln6FYCrdhlswABcLeaQZccZTZw5p-vtwF5sFozZpUuGopsf_n90IC4K7Kzjv5ZJDRYC9doVUrryYPRuOKgF7YNkSbVjP9Qhy_brLqRe9g"
+      },
+      {
+        "name":"Note taker",
+        "description":"a app to write notes",  
+        "imgurl":"https://images-na.ssl-images-amazon.com/images/I/81nia28xsOL._AC_SL1500_.jpg"
+      },
+      {
+        "name":"Weather-dashboard",
+        "description":"weather app",
+        "imgurl":"https://user-images.githubusercontent.com/15653252/71304141-8319f280-2390-11ea-8d60-b072ac8691c7.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Talk is cheap. Show me the code.",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"Roses are red voilets are blue, unexpected { on line 32",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
